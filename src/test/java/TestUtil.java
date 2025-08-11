1eae69fcc4b8f428cf08a5eba27ef6261e6e300a
caketower <dlaqudtn1107@naver.com>
2025-08-11 12:25:02 +0900

    TestUtil 사용(setOutToByteArray(), clearSetOutToByteArray() (출력 흐름제어 테스트))

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -1,8 +1,32 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TestUtil {
+
+    private static PrintStream ORIGIN_OUT = System.out;
+    private static PrintStream CURRENT_OUT = System.out;
+
     public static Scanner genScanner(String input) {
         return new Scanner(input);
 
+    }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        ORIGIN_OUT = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        CURRENT_OUT = printStream;
+
+
+        return  outputStream;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream outputStream) throws IOException { //흐름을 복구 하는 함수
+        System.setOut(ORIGIN_OUT);
+        outputStream.close();
+        CURRENT_OUT.close();
     }
 }
--- src/test/java/TestUtilTest.java
+++ src/test/java/TestUtilTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,5 +30,25 @@ public class TestUtilTest {
         assertThat(saying).isEqualTo("너 자신을 알라");
         assertThat(author).isEqualTo("소크라테스");
 
+    }
+
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray()")
+    void t2() throws IOException {
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        System.out.println("1/ 이순신/ 나의 죽음을 적에게 알리지 마라");    //모니터에 출력되는 문장
+        
+        String outStr = outputStream.toString();    //모니터에 출력되는 문장을 가져옴
+
+        TestUtil.clearSetOutToByteArray(outputStream);  //문장을 가져오고나서 출력 흐름을 정상화함
+
+        //줄바꿈 문자가 하나 추가되어서 isEqualTo로는 통과되지않으니
+        //contain(특정문장을 포함시키면 통과)을 써서 통과시킨다. (줄바꿈이 왜 포함되는것일까?)
+        assertThat(outStr).contains("1/ 이순신/ 나의 죽음을 적에게 알리지 마라");  //모니터에 출력되는 문장을 가져오는게 잘 되는지 확인하는 테스트
+
+
+        System.out.println("이제 화면에 출력됩니다.");
+        
     }
 }
